eeb904b806e5c59572020b9a2be27cf0c1b5f045
seudosan <marco2015.sandoval@gmail.com>
2021-04-25 21:56:21 -0300

    react-router-dom added

---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+function NotFound() {
+    return <h1>Error Not Found</h1>
+}
+
+export default NotFound;
---
+++ src/pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles/Index.css';
+
+class Main extends React.Component {
+    render() {
+        return <h1>Main Page</h1>
+    }
+}
+
+export default Main;
