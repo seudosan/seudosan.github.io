2e9064063f26184bcaaa87e48c55ab1716413b96
seudosan <marco2015.sandoval@gmail.com>
2021-04-25 23:26:20 -0300

    style reset added in index.css with * and BrowserRouter change to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter,
+  HashRouter,
   Switch,
   Route,
   Redirect } from 'react-router-dom';
@@ -10,13 +10,13 @@ import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Index} />
         <Route exact path="/error/not-found/" component={NotFound} />
         <Redirect from="*" to="/error/not-found/" />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
